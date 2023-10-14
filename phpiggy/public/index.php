f40fc90a7bed74e8bb20d902b5224a2bc8a40e62
Timo Wang <timowang1991@gmail.com>
2023-10-14 16:25:27 +0800

    add sugar function for debugging puroposes

--- phpiggy/public/index.php
+++ phpiggy/public/index.php
@@ -1,8 +1,6 @@
 <?php
 
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
+include __DIR__ . "/../src/App/functions.php";
 
 $app = include __DIR__ . "/../src/App/bootstrap.php";
 
---
+++ phpiggy/src/App/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+function dd(mixed $value) {
+    echo "<pre>";
+    print_r($value);
+    echo "</pre>";
+    die(); // stops to generate the page for quick debugging purpose
+}
+
+?>
