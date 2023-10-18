1fe48aaec07f2eb7ef07803c5c1fa01228c1fe14
Timo Wang <timowang1991@gmail.com>
2023-10-18 21:58:44 +0800

    move route registration to separate file

--- phpiggy/src/App/bootstrap.php
+++ phpiggy/src/App/bootstrap.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 include __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\{HomeController, AboutController};
+use function App\Config\registerRoutes;
 
 $app = new App();
-
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/about', [AboutController::class, 'about']);
+registerRoutes($app);
 
 // dd($app);
 
