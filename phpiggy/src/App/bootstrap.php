94af6db31f6e922f3257ca43ac57e691c59dde4e
Timo Wang <timowang1991@gmail.com>
2023-10-18 21:42:55 +0800

    add about page

--- phpiggy/src/App/bootstrap.php
+++ phpiggy/src/App/bootstrap.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 include __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 
 $app = new App();
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 // dd($app);
 
-return $app
-
-?>
+return $app;
