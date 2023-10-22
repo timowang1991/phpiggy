783e08d882e9f569419df98958c1a6227c4f3152
Timo Wang <timowang1991@gmail.com>
2023-10-22 18:05:00 +0800

    instantiate class with dependency injection in container

--- phpiggy/src/App/Controllers/HomeController.php
+++ phpiggy/src/App/Controllers/HomeController.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
-use App\Config\Paths;
 
 class HomeController
 {
-    private TemplateEngine $view;
-
-    public function __construct()
-    {
-        $this->view = new TemplateEngine(Paths::VIEW);
+    public function __construct(
+        private TemplateEngine $view
+    ) {
     }
 
     public function home()
--- phpiggy/src/Framework/Container.php
+++ phpiggy/src/Framework/Container.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Framework;
 
-use ReflectionClass;
+use ReflectionClass, ReflectionNamedType;
 use Framework\Exceptions\ContainerException;
 
 class Container
@@ -24,7 +24,45 @@ class Container
             throw new ContainerException("Class {$className} is not instantiable");
         }
 
+        $constructor = $reflectionClass->getConstructor();
 
-        dd($reflectionClass);
+        if (!$constructor) {
+            return new $className;
+        }
+
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $className;
+        }
+
+        $dependencies = [];
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+            if (!$type) {
+                throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+            }
+
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerException("Failed to resolve class {$className} because param {$name} has an invalid type hint.");
+            }
+
+            $dependencies[] = $this->get($type->getName());
+        }
+
+        return $reflectionClass->newInstanceArgs($dependencies);
+    }
+
+    public function get(string $id)
+    {
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ContainerException("Class {$id} does not exist in container.");
+        }
+
+        $factory = $this->definitions[$id];
+        $dependency = $factory();
+
+        return $dependency;
     }
 }
